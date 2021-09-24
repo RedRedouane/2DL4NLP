6854b591df93521294a06bd3fc043291983918b9
Redouane Dahmani <redouane1997@hotmail.com>
2021-09-24 13:48:48 +0200

    jobbert

---
+++ code/jobbert.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#SBATCH --partition=gpu_shared_course
+#SBATCH --gres=gpu:1
+#SBATCH --job-name=test_JOEYNMT
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --time=1:00:00
+#SBATCH --mem=32000M
+#SBATCH --output=test_JOEYNMT.out
+
+module purge
+module load 2019
+module load Python/3.7.5-foss-2019b
+module load CUDA/10.1.243
+module load cuDNN/7.6.5.32-CUDA-10.1.243
+module load NCCL/2.5.6-CUDA-10.1.243
+# module load Anaconda3/2018.12
+
+# Your job starts in the directory where you call sbatch
+cd $HOME/2DL4NLP/code/
+# Activate your environment
+source jnmt/bin/activate
+# Run your code
+srun python -u -m joeynmt train configs/test_gpu.yaml
